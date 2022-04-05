f4ce288a48b3d8a192c872210d5718865ae327c1
frederic <frederic.jaimes@ratp.fr>
2022-04-05 16:19:52 +0100

    dev : mise en place typescript ts

---
+++ back/server.ts
@@ -0,0 +1,20 @@
+console.log("About the server");
+
+const express = require("express");
+const serveIndex = require("serve-index"); //gestion des repertoires
+
+const app = express();
+const port = 3000;
+const wwwDir = ".";
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
