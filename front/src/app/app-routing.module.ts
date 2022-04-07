f1eb8e17e53599b8f2c46f4c1c38c87fed35197f
frederic <frederic.jaimes@ratp.fr>
2022-04-07 08:50:22 +0100

    dev: chargement en background de ressources

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {
+  PreloadAllModules,
+  PreloadingStrategy,
+  RouterModule,
+  Routes,
+} from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
 import { LegaleComponent } from './routes/legale/legale.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legale', component: LegaleComponent },
-  { path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule) },
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./stock/stock.module').then((m) => m.StockModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
