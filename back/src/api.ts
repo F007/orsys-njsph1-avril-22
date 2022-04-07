cb7d3b939b88f4bfb775f017960bdcc1e2c92cf1
frederic <frederic.jaimes@ratp.fr>
2022-04-07 13:30:41 +0100

    dev: add groupform

--- back/src/api.ts
+++ back/src/api.ts
@@ -2,7 +2,26 @@ import { Router } from "express";
 import { Articles } from "./interfaces/Articles";
 const app = Router();
 
-const articles: Articles[] = [{ id: 12, name: "marteau", price: 11, qty: 10 }];
+const articles: Articles[] = [
+  {
+    id: 12,
+    name: "marteau",
+    price: 11,
+    qty: 10,
+  },
+  {
+    id: 13,
+    name: "clou",
+    price: 55,
+    qty: 10,
+  },
+  {
+    id: 14,
+    name: "agraff",
+    price: 96,
+    qty: 10,
+  },
+];
 
 app.get("/crash", (req, res, next) => {
   (async () => {
--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('default', [Validators.required]),
+    price: new FormControl('default', [Validators.required]),
+    qty: new FormControl('default', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
