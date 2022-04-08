e806bac40a78b2a03d371c20156574915eb4849a
frederic <frederic.jaimes@ratp.fr>
2022-04-08 14:15:20 +0100

    dev: modif des tests avec axios en commentaire a revoir car fonctionne pas

--- back/test/test.spec.ts
+++ back/test/test.spec.ts
@@ -1,10 +1,16 @@
 import assert from "assert";
 import { WebServer } from "../WebServer";
+import axios from "axios";
+import { count } from "console";
+
 describe("Server", function () {
   it("should start and stop", async () => {
-    process.env.PORT = "3232";
+    process.env.PORT = "3000";
     const server = new WebServer();
     await server.start();
     await server.stop();
+    //const result = await axios.get("http://localhost:3232/api/datte");
+    //console.log(result.data);
+    //assert.ok(result.data.date);
   });
 });
